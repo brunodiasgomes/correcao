c676d6a9f57e2666c4348b44088762dee4414673
brunodiasgomes <42781509+brunodiasgomes@users.noreply.github.com>
2018-10-17 16:03:33 -0300

    Add files via upload

---
+++ Correcao/IMC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Correcao /* Aluno: Bruno Dias Gomes RA 31827615*/
+{    /*Questão 02*/
+    class IMC
+    {    /*Primeira Forma*/
+        private double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+
+        }
+        /*Segunda Forma EX*/
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+        public double Classificar(double imc)
+        {
+            return 0.0;
+        }
+      
+    }
+}
---
+++ Correcao/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correcao /* Aluno: Bruno Dias Gomes RA 31827615*/
+{
+    class Program
+    {    /*Questão 01*/
+        static int Maior(int a, int b) /*Primeira Forma*/
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+         /*Questão 01*/
+        static int Maior2(int a, int b) /*Segunda Forma*/
+        {
+            return (a > b) ? a : b;
+        }
+          static void Main(string[] args)
+        {
+            Console.WriteLine(Maior(1, 2));
+            Console.WriteLine(Maior2(3, 4));
+            Console.ReadKey();
+        }
+        /*Questão 03*/
+        static bool Par(int numero) /* Primeira Forma*/
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return true;
+            else
+                return false;
+        }
+        /*Questão 03*/
+        static string Par2(int numero) /* Segunda Forma*/
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return "Par";
+            else
+                return "Ímpar";
+        }
+    }
+}
+            
